d97e164d148d01227f6d594371774e60f2cdca55
Javier <jglete@eskura21.com>
2021-06-18 10:25:25 +0200

    Directiva estructural unless

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appUnless]'
 })
 export class UnlessDirective {
+  private hasView = false;
 
-  constructor() { }
+  @Input() set appUnless(condition: boolean) {
+    if (!condition && !this.hasView) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.hasView = true;
+    } else if (condition && this.hasView) {
+      this.viewContainer.clear();
+      this.hasView = false;
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef) { }
 
 }
